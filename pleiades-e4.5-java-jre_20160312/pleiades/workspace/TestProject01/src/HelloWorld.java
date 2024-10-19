ae81a3f264aa01d291851656846c90e6aa632d4c
Yu-ka0912 <aoyamayuka.0912@gmail.com>
2024-10-19 22:32:54 +0900

    Java

--- pleiades-e4.5-java-jre_20160312/pleiades/workspace/TestProject01/src/HelloWorld.java
+++ pleiades-e4.5-java-jre_20160312/pleiades/workspace/TestProject01/src/HelloWorld.java
@@ -4,7 +4,12 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		//Hello World!の出力
 		System.out.println("Hello World!");
-
+		
+		String abc="山田";
+		System.out.println(abc +"さん");
+		
+		float a=(float)10/3;
+		System.out.println(a);
 	}
 
 }
